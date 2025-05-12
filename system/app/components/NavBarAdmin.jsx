8fbf2c02dfd56cb163e3e1604680c5c581f27ab2
evwp <me2205158@qu.edu.qa>
2025-05-12 22:02:57 +0300

    last

--- system/app/components/NavBarAdmin.jsx
+++ system/app/components/NavBarAdmin.jsx
@@ -12,36 +12,28 @@ export default function NavBarAdmin() {
     router.push("/");
   };
 
-  return (
-    <header className="navbar-admin">
-      <div className="navbar-container">
-        <div className="navbar-left">
-          <img src="/media/cse-logo.png" alt="QU CSE" className="navbar-logo" />
-        </div>
 
-        <h1 className="navbar-title-center">CSE Admin Dashboard</h1>
-
-        <nav className="navbar-nav">
-          <li>
-            <Link href="/System/admin" className="nav-link">
-              <i className="fas fa-home"></i> Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/System/admin/create-course" className="nav-link">
-              <i className="fas fa-plus-circle"></i> Create Course
-            </Link>
-          </li>
-          <li>
-            <Link href="/System/admin/status" className="nav-link">
-              <i className="fas fa-chart-bar"></i> Status
-            </Link>
-          </li>
+    return (
+      <header className="navbar-admin">
+        <div className="navbar-top">
+          <img src="https://media-hosting.imagekit.io/a086cbbc74ab43c8/qu-logo-small.png?Expires=1841677972&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=Gi5xdMBev1avYk7RXcCH1IUxJBtA1rjPYtAe7PXwVTkYgNWHCKMUn9wQ~A8e04-J4Ln5PbAkpwj~2TMzqkCIzlKG0XQtjk7OP5IOWXnk3T9gcH719uDn762juKiFQjfwzdXrnSt-wdP31e9gLyq~VFWecfMkaVoka2FzmFQrmJZk-z~5YEngLp43tAojpmVGojs2lABB9K72rxkJXe3YurgdlDevjXzryIw6jIIeyH~4CwFlh6V7XjgQzOlNUCecgoIodVO3VNEhJQKv8tXHYl9ayGITaC6IU-~jiv9zwCvwkpW9TeX9hI~tC0Ag126OJXf5Wa07WrvxwKONBhIJsg__" alt="QU CSE" className="navbar-logo" />
+          <h1 className="navbar-title">CSE Admin Dashboard</h1>
           <button onClick={handleLogout} className="logout-button">
             <i className="fas fa-sign-out-alt"></i> Logout
           </button>
+        </div>
+   
+        <nav className="navbar-links">
+          <Link href="/System/admin" className="nav-btn">
+            <i className="fas fa-home"></i> Dashboard
+          </Link>
+          <Link href="/System/admin/create-course" className="nav-btn">
+            <i className="fas fa-plus-circle"></i> Create Course
+          </Link>
+          <Link href="/System/status" className="nav-btn">
+            <i className="fas fa-chart-bar"></i> Show Statistics
+          </Link>
         </nav>
-      </div>
-    </header>
-  );
-}
+      </header>
+    );
+  }
